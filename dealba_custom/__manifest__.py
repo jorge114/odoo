de0a22346b6b9e30489ebee6a731f19be2ff620c
Juan Pablo Calderon Gallardo <jcalderon@kuh7.mx>
2021-12-13 18:21:05 -0600

    custom

---
+++ dealba_custom/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'KUH7 - Dealba Custom',
+    'summary': 'KUH7 - Dealba Custom',
+    'description': '''
+    KUH7 - Dealba Custom
+    ''',
+    "website": "https://www.kuh7.mx/",
+    'author': 'KUH7 SOLUCIONES S.A. de C.V.',
+    'version': '1.0',
+    'category': 'Dealba',
+    'depends': [
+        'project',
+        'hr',
+        'sale_management',
+        'sale_project',
+    ],
+    'data': [
+        'data/project_task_type_data.xml',
+        'security/ir.model.access.csv',
+        'views/project_project_views.xml',
+        'views/project_task_views.xml',
+        'views/project_views.xml',
+        'views/assets.xml',
+        'report/report_proyecto_avance.xml',
+    ],
+}
---
+++ dealba_custom/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import project_project
+from . import project_task
+from . import res_partner
