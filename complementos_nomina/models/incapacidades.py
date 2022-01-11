5e452e6bb20bea801690157ca36312c3283f4117
Juan Pablo Calderon Gallardo <jcalderon@kuh7.mx>
2022-01-10 18:50:12 -0600

    incapacidades

--- complementos_nomina/models/incapacidades.py
+++ complementos_nomina/models/incapacidades.py
@@ -70,7 +70,7 @@ class IncapacidadesNomina(models.Model):
 
             holiday = holidays_obj.new(vals)
             #holiday._onchange_employee_id()
-            holiday._onchange_leave_dates()
+            #holiday._onchange_leave_dates()
             vals.update(holiday._convert_to_write({name: holiday[name] for name in holiday._cache}))
             vals.update({'holiday_status_id': leave_type and leave_type.id, })
             incapacidad = self.env['hr.leave'].create(vals)
