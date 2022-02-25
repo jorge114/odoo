df618b48f81dd95a56e7308f8bbbb97a56634145
Juan Pablo Calderon Gallardo <jcalderon@kuh7.mx>
2022-02-24 18:06:04 -0600

    dealba_custom_new

---
+++ dealba_custom_new/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Dealba Custom',
+    'summary': 'Dealba Custom',
+    'description': '''
+    KUH7 - Dealba Custom
+    ''',
+    "website": "https://www.kuh7.mx/",
+    'author': 'KUH7 SOLUCIONES S.A. de C.V.',
+    'version': '1.0',
+    'category': 'Dealba',
+    'depends': [
+        'project',
+        'dealba_custom',
+       
+    ],
+    'data': [
+        
+        'views/assets_custom.xml',
+        'views/proyect_task_view_custom.xml',
+       
+    ],
+}
